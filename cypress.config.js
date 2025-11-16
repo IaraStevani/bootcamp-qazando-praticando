8642dc4a52391bb6c08b50347e7a3a92cc1b60a9
IaraStevani <iarastevanialves@gmail.com>
2025-11-15 22:42:23 -0300

    Configurando relatório mochawesome para o projeto Praticando

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '15qp1h',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts:true,
+      reportTitle: 'Bootcamp QAzando - Praticando',
+      reportPageTitle: 'Bootcamp QAzando - Praticando',
+    },
     baseUrl: 'https://automationpratice.com.br/',
     defaultCommandTimeout: 5000,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
